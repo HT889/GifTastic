295a8a6aa50b2df0c9780d7ff660991af81131ca
Haig Talarian <haigtalarian@gmail.com>
2019-06-25 09:52:02 -0700

    some styling, added div containers for gifs for styling

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
-var topics = [`piano`, `subaru-wrx`, `los angeles`, `pink floyd`, `shell fish`];  //first set of buttons come from here
+var topics = [`pianos`, `subaru-wrx`, `los angeles`, `pink floyd`, `shell fish`, `armenia`];  //first set of buttons come from here
 
-var totalGifs = "&limit=" + 11;
+var totalGifs = "&limit=" + 10;
 var api = 'https://api.giphy.com/v1/gifs/search?&q='
 var apiKey = '&api_key=xFgIECuKdXjNZ8CSmnrEFOtdyDpjj6tx'
 var urlMain = api + apiKey + totalGifs;
@@ -31,15 +31,18 @@ function populateGIFContainer(input){
         var data = response.data;
         
         for(var i = 0; i < data.length; i++) {      //for loop to generate images and attached ratings
-            var rating = "Rating: " + data[i].rating.toUpperCase();
+            var rating = "<p>Rating: " + data[i].rating.toUpperCase() + "</p>";
             // console.log(rating);
             var images = $("<img>");
+            var  imagesDiv = $("<div>");
+            imagesDiv.attr("class", "imagesDiv");
             images.attr("src", data[i].images.fixed_height_still.url);
             images.attr("class", "gifs-class");
             images.attr("data-still", data[i].images.fixed_height_still.url);
             images.attr("data-animate", data[i].images.fixed_height.url);
             images.attr("data-state", "still");
-            $("#gif-container").append(rating, images);
+            imagesDiv.append(rating, images);
+            $("#gif-container").append(imagesDiv);
            
         }
     })};
@@ -64,16 +67,30 @@ function populateGIFContainer(input){
 // }
 $(document).ready(function(){       //when the webpage loads, begin here:
 createButtons();
+// $("#formieform").submit(function(e) {
+//     e.preventDefault();
+// $("#cool-stuff-search").keypress(function(event) {
+//     var searchBar = $("#cool-stuff-search").val();
+//     if (event.which == 13 && topics.includes(searchBar) === false) {
+//         event.preventDefault();
+//         topics.push(searchBar);
+//         createButtons();     
+//     }
+//  });
+    
+// });
 // console.log(urlMain);
 // console.log(totalGifs);
 // console.log(input);
-$("#submit").on("click", function(){
+$("#submit").on("click", function (){
+    event.preventDefault();
     var searchBar = $("#cool-stuff-search").val();
     // console.log(searchBar);
     if (topics.includes(searchBar) === false) {   //only make new buttons if desired button doesnt already exist, preventing duplicates 
         topics.push(searchBar);
         createButtons();     
     }
+    
 });
 
 $("#gif-container").on("click", ".gifs-class", function(){      //for starting and stopping animatio on GIF's by clicking on them
